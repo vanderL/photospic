75af45eabdd5934503610ac0cf975d8c4e6c3280
Vander Lima <vnd.vander@gmail.com>
2017-02-14 23:42:04 -0300

    Css encapsulado e FotoService

---
+++ client/app/foto/foto.service.ts
@@ -0,0 +1,31 @@
+import {Http, Headers, Response} from '@angular/http';
+import { FotoComponent } from './foto.component';
+import {Observable} from 'rxjs';
+
+
+export class FotoService {
+
+    http: Http;
+    headers: Headers;
+    url: string = 'v1/fotos';
+
+    constructor(http: Http) {
+      this.http = http;
+      this.headers = new Headers();
+      this.headers.append('Content-Type', 'application/json');
+    }
+
+     lista(): Observable<FotoComponent[]> {
+
+       return this.http.get(this.url)
+            .map(res => res.json());
+     }
+
+     cadastrar(foto: FotoComponent): Observable<Response> {
+
+       return this.http.post(this.url, JSON.stringify(foto),
+                { headers: this.headers });
+
+     }
+
+}
