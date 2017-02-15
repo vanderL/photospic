67ec9811b1b917772717c62de689045c40f4fcce
Vander Lima <vnd.vander@gmail.com>
2017-02-15 01:11:18 -0300

    Button remover

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -12,5 +12,6 @@ export class FotoComponent {
     @Input() titulo: string = '';
     @Input() url: string = '';
     descricao: string = '';
+    _id: string;
 
 }
--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -11,13 +11,36 @@ import { FotoService } from '../foto/foto.service';
 export class ListagemComponent {
 
     fotos: FotoComponent[] = [];
+    service: FotoService;
+    mensagem: string = '';
+
 
     constructor(service: FotoService) {
-
+      this.service = service;
         service.lista()
             .subscribe(
                 fotos => this.fotos = fotos,
                 erro => console.log(erro)
             );
+    }
+
+    remove(foto) {
+
+        this.service
+            .remove(foto)
+            .subscribe(
+                fotos => {
+                    let novasFotos = this.fotos.slice(0);
+                    let indice = novasFotos.indexOf(foto);
+                    novasFotos.splice(indice, 1);
+                    this.fotos = novasFotos;
+                    this.mensagem = 'Foto removida com sucesso!';
+                },
+                erro => {
+                    console.log(erro);
+                    this.mensagem = "Erro: " + erro;
+                }
+            );
+
     }
 }
